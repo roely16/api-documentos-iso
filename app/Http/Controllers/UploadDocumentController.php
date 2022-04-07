e62110dcaf97737520b1ac7991516f36e44e4c96
Roely Chur <gerson.roely@gmail.com>
2022-04-07 09:46:14 -0600

    documentos baja

--- app/Http/Controllers/UploadDocumentController.php
+++ app/Http/Controllers/UploadDocumentController.php
@@ -42,7 +42,7 @@
 				
 				$empleado = Empleado::find($request->nit);
 
-				$documentos_revision = DocumentoRevision::where('CODAREA', $empleado->codarea)->where('PARENT_DOCUMENTOID', null)->orderBy('DOCUMENTOID', 'desc')->get();
+				$documentos_revision = DocumentoRevision::where('CODAREA', $empleado->codarea)->where('PARENT_DOCUMENTOID', null)->where('BAJA', '0')->orderBy('DOCUMENTOID', 'desc')->get();
 
 				foreach ($documentos_revision as &$documento) {
 					
